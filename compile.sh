e46465957d1d92b21ed9c4c07f2a5552098f37d1
faisal.mohammad.00109781 <faisal.mohammad@jhuapl.edu>
2020-11-18 18:39:12 -0500

    compile script

---
+++ compile.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+  
+# In the process of figuring out how Maven works,
+# therefore in the meantime, we use this script to
+# do a quick compile.
+javac src/ether/*.java src/links/*.java src/utils/*.java
