29e7067432ebae7fb1b77f3f364cd1a0897d3d59
Zheng Sun <zhengsun@Zhengs-MacBook-Pro.local>
2020-11-26 16:16:55 -0500

    client minion registry bind function added

--- src/ether/Minion.java
+++ src/ether/Minion.java
@@ -98,12 +98,12 @@ public class Minion extends UnicastRemoteObject implements MasterMinionLink, Min
 
 			// need to bind on master
 			MasterMinionLink mm_stub = (MasterMinionLink) UnicastRemoteObject.toStub(this);
-			jumpLink.registryBind(registry, "MasterMinionLink_" + this.minionID, mm_stub);
+			masterLink.registryBind(registry, "MasterMinionLink_" + this.minionID, mm_stub);
 			//registry.rebind("MasterMinionLink_" + this.minionID, mm_stub);
 			
 			//start a new registry on Minion
 			ClientMinionLink cm_stub = (ClientMinionLink) UnicastRemoteObject.toStub(this);
-			registry.rebind("ClientMinionLink_" + this.minionID, cm_stub);
+			masterLink.registryBind(registry, "ClientMinionLink_" + this.minionID, cm_stub);
 
 		} catch (RemoteException | NotBoundException e) {
 			e.printStackTrace();
--- src/links/ClientMinionLink.java
+++ src/links/ClientMinionLink.java
@@ -16,8 +16,6 @@ public interface ClientMinionLink extends Remote {
 	
 	void createDir(String dirName, FileNode cwd) throws RemoteException;
 	
-	public void registryBind(Registry registry, String name, ClientMinionLink link);
-	
 //	void readFile(String filename) throws RemoteException;
 
 }
