3fa702b65c28b0061cf61bab82ca5237c0ea7638
Zheng Sun <zhengsun@Zhengs-MacBook-Pro.local>
2020-11-27 13:29:22 -0500

    debugging msg

--- src/ether/Minion.java
+++ src/ether/Minion.java
@@ -70,6 +70,7 @@ public class Minion extends UnicastRemoteObject implements MasterMinionLink, Cli
 			LocateRegistry.createRegistry(REG_PORT + 1 + this.minionID);
 			System.out.println("Registry instance exported on port: " + REG_PORT + 1 + this.minionID);
 			minionRegistry = LocateRegistry.getRegistry(REG_ADDR, REG_PORT + 1 + this.minionID);
+			System.out.println("minion registry get");
 
 			MasterMinionLink mm_stub = (MasterMinionLink) UnicastRemoteObject.toStub(this);
 			minionRegistry.rebind("MasterMinionLink_" + this.minionID, mm_stub);
