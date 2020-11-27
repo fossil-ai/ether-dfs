33d6c29e6efc259895112146c5a8e01068c65a7e
Zheng Sun <zhengsun@Zhengs-MacBook-Pro.local>
2020-11-27 13:39:03 -0500

    add parentheses

--- src/ether/Minion.java
+++ src/ether/Minion.java
@@ -68,8 +68,8 @@ public class Minion extends UnicastRemoteObject implements MasterMinionLink, Cli
 			
 			System.out.println("Creating Java RMI registry for minion as well");
 			LocateRegistry.createRegistry(REG_PORT + 1 + this.minionID);
-			System.out.println("Registry instance exported on port: " + REG_PORT + 1 + this.minionID);
-			minionRegistry = LocateRegistry.getRegistry(REG_ADDR, REG_PORT + 1 + this.minionID);
+			System.out.println("Registry instance exported on port: " + (REG_PORT + 1 + this.minionID));
+			minionRegistry = LocateRegistry.getRegistry(REG_ADDR, (REG_PORT + 1 + this.minionID));
 			System.out.println("minion registry get");
 
 			MasterMinionLink mm_stub = (MasterMinionLink) UnicastRemoteObject.toStub(this);
