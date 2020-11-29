305b4de9bfb9550111071e2f300dc540bfdc5760
Zheng Sun <zhengsun@Zhengs-MacBook-Pro.local>
2020-11-29 18:59:22 -0500

    sdas

--- src/ether/Client.java
+++ src/ether/Client.java
@@ -136,7 +136,9 @@ public class Client {
 				try {
 					FileContent content = new FileContent(cmds[1]);
 					System.out.println("current minion mem space is used %" + client.minionLink.getMemSpace());
-				    while (client.minionLink.getMemSpace() < 0.2 ) {
+					System.out.println("current minion mem space is used %" + client.minionLink.getMemSpace());
+					System.out.println("current minion mem space is used %" + client.minionLink.getMemSpace());
+				    while (client.minionLink.getMemSpace() > 0.2 ) {
 						System.out.println("not enough space on this minion Server");
 						System.out.println("moving to another minion Server");
 						client.minionLink = client.nextMinionLink;
