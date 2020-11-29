18597df963cd1e002546c9f4a306fc087f3bc2cf
Zheng Sun <zhengsun@Zhengs-MacBook-Pro.local>
2020-11-29 16:11:19 -0500

    wdqwdwq

--- src/ether/Minion.java
+++ src/ether/Minion.java
@@ -183,17 +183,11 @@ public class Minion extends UnicastRemoteObject implements MasterMinionLink, Cli
 		{
 			System.out.println("current minion capacity is "  + getMemSpace());
 			System.out.println("current minion has reached capacity, move to next minion");
-			String ip;
-			try {
-				ip = InetAddress.getLocalHost().getHostAddress();
-			} catch (UnknownHostException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			minionRegistry = LocateRegistry.getRegistry(ip, (50903 + 1 + this.minionID));
+			ConfigReader reader = new ConfigReader();
+			minionRegistry = LocateRegistry.getRegistry(reader.getMinion2Addr(), (50903 + 1 + this.minionID));
 			System.out.println("registry get");
 			System.out.println("rebind next");
-			minionRegistry.rebind("MinionMinionLink_" + this.minionID, UnicastRemoteObject.toStub(this));
+			//minionRegistry.rebind("MinionMinionLink_" + this.minionID, (MinionMinionLink) UnicastRemoteObject.toStub(this));
 			System.out.println("the MinionMinion Link is:  " + "MinionMinionLink_" + this.minionID);
 			try {
 				minionMinionLink = (MinionMinionLink) minionRegistry.lookup("MinionMinionLink_" + this.minionID);
