a3d559009f2536244ce7a7ddecabab45900116cf
faisal.mohammad.00109781 <faisal.mohammad@jhuapl.edu>
2020-11-30 00:12:39 -0500

    cleaned up config parser

--- src/utils/ConfigReader.java
+++ src/utils/ConfigReader.java
@@ -10,19 +10,30 @@ import java.util.TreeMap;
 public class ConfigReader {
 
 	private Map<String, String> configs;
+	private Map<String, Map<String, String>> minionConfigs;
 	private BufferedReader reader;
-	
+
 	public ConfigReader() {
 
 		try {
 			reader = new BufferedReader(new FileReader("resources/filesys.conf"));
 			configs = new TreeMap<String, String>();
+			minionConfigs = new TreeMap<String, Map<String, String>>();
 			String currentLine;
 			while ((currentLine = reader.readLine()) != null) {
+				System.out.println(currentLine);
 				String[] config = currentLine.split(" ");
-				configs.put(config[0], config[1]);
+				if(config[0].equalsIgnoreCase("MINION_ADDRESS_PORT")) {
+					String[] info = config[1].split(",");
+					TreeMap<String, String> minion_info = new TreeMap<String, String>();
+					minion_info.put("minionHost", info[1]);
+					minion_info.put("minionPort", info[2]);
+					this.minionConfigs.put(info[0], minion_info);
+				}
+				else {
+					configs.put(config[0], config[1]);
+				}
 			}
-			
 
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -38,35 +49,17 @@ public class ConfigReader {
 		return configs.get("REG_ADDRESS");
 	}
 
-	public String getRegistryClientJumpName() {
-		return configs.get("MS_CLIENT_JUMPLINK");
-	}
-
-	public String getRegistryMinionJumpName() {
-		return configs.get("MS_MINION_JUMPLINK");
-	}
 
 	public int getMinionNum() {
 		return Integer.parseInt(configs.get("MINION_NUMS"));
 	}
-
-	public String getMinion1Addr() {
-		return configs.get("MINION_ADDRESS1");
+	
+	public String getMinionHost(String id) {
+		return this.minionConfigs.get(id).get("minionHost");
 	}
-	public String getMinion2Addr() {
-		return configs.get("MINION_ADDRESS2");
-	}
-	public String getMinion3Addr() {
-		return configs.get("MINION_ADDRESS3");
-	}
-	public int getMinion1Port() {
-		return Integer.parseInt(configs.get("MINION_PORT1"));
-	}
-	public int getMinion2Port() {
-		return Integer.parseInt(configs.get("MINION_PORT2"));
-	}
-	public int getMinion3Port() {
-		return Integer.parseInt(configs.get("MINION_PORT3"));
+	
+	public int getMinionPort(String id) {
+		return Integer.parseInt(this.minionConfigs.get(id).get("minionPort"));
 	}
 
 
@@ -74,11 +67,18 @@ public class ConfigReader {
 		ConfigReader reader = new ConfigReader();
 		System.out.println(reader.getRegistryPort());
 		System.out.println(reader.getRegistryHost());
-		System.out.println(reader.getRegistryClientJumpName());
-		System.out.println(reader.getRegistryMinionJumpName());
 		System.out.println(reader.getMinionNum());
-		System.out.println(reader.getMinion1Addr());
-		System.out.println(reader.getMinion2Addr());
-		System.out.println(reader.getMinion3Addr());
+		
+		System.out.println(reader.getMinionHost("0"));
+		System.out.println(reader.getMinionPort("0"));
+		
+		System.out.println(reader.getMinionHost("1"));
+		System.out.println(reader.getMinionPort("1"));
+		
+		System.out.println(reader.getMinionHost("2"));
+		System.out.println(reader.getMinionPort("2"));
+		
+		System.out.println(reader.getMinionHost("3"));
+		System.out.println(reader.getMinionPort("3"));
 	}
 }
